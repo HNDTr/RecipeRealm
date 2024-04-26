01f2501e20b4abc1b8bd27abaa8f45f8b01fa6d2
Huy Tran <htran@middlebury.edu>
2024-04-26 15:58:45 -0400

    pos changes

--- src/pages/create.js
+++ src/pages/create.js
@@ -3,35 +3,33 @@ import PropTypes from "prop-types";
 import RecipeCreator from "../components/RecipeCreator";
 
 export default function Creator({ pushCurrentRecipe }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    const completeFunction = async (recipe) => {
-        if (recipe) {
-            const response = await fetch(`/api/articles`, {
-                method: "POST",
-                body: JSON.stringify(recipe),
-                headers: new Headers({
-                   Accept: "application/json",
-                   "Content-Type": "application/json",
-                }),
-             });
-            
-            if (response.ok){
-                const returnRecipe = await response.json();
-                pushCurrentRecipe(returnRecipe);
-            } 
-        } else {
-            router.back();
-        }
-    };
+  const completeFunction = async (recipe) => {
+    if (recipe) {
+      const response = await fetch(`/api/recipes`, {
+        method: "POST",
+        body: JSON.stringify(recipe),
+        headers: new Headers({
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        }),
+      });
 
-    return(
-        <RecipeCreator
-            completeFunction={(recipe) => completeFunction(recipe)}
-        />
-    );
+      if (response.ok) {
+        const returnRecipe = await response.json();
+        pushCurrentRecipe(returnRecipe);
+      }
+    } else {
+      router.back();
+    }
+  };
+
+  return (
+    <RecipeCreator completeFunction={(recipe) => completeFunction(recipe)} />
+  );
 }
 
 Creator.propTypes = {
-    pushCurrentRecipe: PropTypes.func.isRequired,
-  };
+  pushCurrentRecipe: PropTypes.func.isRequired,
+};
