f1234585c550cc3002a6a3938d93e24469d18ff5
Eric Tran <etran@middlebury.edu>
2024-05-11 09:43:13 -0400

    log in works now, 2 eslint errors

--- knex/migrations/20240416234842_user_recipes.js
+++ knex/migrations/20240416234842_user_recipes.js
@@ -2,23 +2,25 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-/* eslint-disable func-names */
 exports.up = function (knex) {
   // this is the join table that will be used to hold the relationship between recipes and users (many-to-many relationship)
   // this is not the author of the recipe, but rather the users that have saved the recipe to their list
-  return knex.schema.createTable("recipe_user", (table) => {
-    table
-      .integer("recipe_id")
-      .references("id")
-      .inTable("recipes")
-      .onDelete("CASCADE");
-    table
-      .integer("user_id")
-      .references("id")
-      .inTable("users")
-      .onDelete("CASCADE");
+  // Create the recipe_user table
+  const createRecipeUserTable = knex.schema.createTable("recipe_user", (table) => {
+    table.integer("recipe_id").references("id").inTable("recipes").onDelete("CASCADE");
+    table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
     table.primary(["recipe_id", "user_id"]);
   });
+
+  // Create the User table
+  const createUserTable = knex.schema.createTable("User", (table) => {
+    table.increments("id").primary();
+    table.string("googleId");
+    table.string("name");
+    table.text("email");
+  });
+
+  return Promise.all([createRecipeUserTable, createUserTable]);
 };
 
 /**
@@ -26,5 +28,8 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("recipe_user");
+  const dropUserTable = knex.schema.dropTableIfExists("User");
+  const dropRecipeUserTable = knex.schema.dropTableIfExists("recipe_user");
+
+  return Promise.all([dropUserTable, dropRecipeUserTable]);
 };
--- models/User.js
+++ models/User.js
@@ -1,45 +1,40 @@
-/* eslint-disable camelcase */
+// User.js
 import { Model } from "objection";
-// import { use } from "react";
 import BaseModel from "./BaseModel";
-import Recipe from "./Recipe"; // eslint-disable-line import/no-cycle
+import Recipe from "./Recipe";
 
 export default class User extends BaseModel {
-  // Table name is the only required property.
   static get tableName() {
-    return "users";
+    return "User";
   }
 
   static get jsonSchema() {
     return {
       type: "object",
-      required: ["username"],
+      required: ["googleId"],
       properties: {
         id: { type: "integer" },
-        username: { type: "string" },
-        password: { type: "string" },
+        googleId: { type: "string" },
+        name: { type: "string" },
         email: { type: "string" },
-        created: { type: "date-time" },
       },
     };
   }
 
   static relationMappings = () => ({
     recipes: {
-      // schema for join table between recipes and ingredients (many-to-many relationship)
       relation: Model.HasManyRelation,
       modelClass: Recipe,
       join: {
-        from: "users.id",
+        from: "User.id",
         to: "recipes.user_id",
       },
     },
     savedRecipes: {
-      // schema for join table between tags and recipes (many-to-many relationship)
       relation: Model.ManyToManyRelation,
       modelClass: Recipe,
       join: {
-        from: "users.id",
+        from: "User.id",
         through: {
           from: "recipe_user.user_id",
           to: "recipe_user.recipe_id",
@@ -48,4 +43,11 @@ export default class User extends BaseModel {
       },
     },
   });
+
+  // Override this method to exclude googleId
+  $formatJson(json) {
+    const formattedJson = super.$formatJson(json);
+    delete formattedJson.googleId;
+    return formattedJson;
+  }
 }
